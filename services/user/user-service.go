c6720be79111aba5f9b2a1f1ef803b191808fd8e
Sri Harsha Kappala <sri.kappala@paytm.com>
2023-11-27 20:53:08 +0530

    Fix indendation in user service

--- services/user/user-service.go
+++ services/user/user-service.go
@@ -20,34 +20,34 @@ func AddUser(db *sql.DB) error {
 	userCreateStmt, err := db.Prepare("INSERT INTO users (username) VALUES (?)")
 	if err != nil {
 		fmt.Println(err)
-        return err
+			return err
     }
-    defer userCreateStmt.Close()
+	defer userCreateStmt.Close()
 
 	result, err := userCreateStmt.Exec(username)
-    if err != nil {
+	if err != nil {
 		fmt.Println(err)
-        return err
-    }
+		return err
+	}
 
 	lastInsertID, err := result.LastInsertId()
 
 	walletCreateStmt, err := db.Prepare("INSERT INTO wallet (user_id, balance) VALUES (?, ?)")
 	if err != nil {
 		fmt.Println(err)
-        return err
-    }
-    defer walletCreateStmt.Close()
+		return err
+	}
+	defer walletCreateStmt.Close()
 
 	_, err = walletCreateStmt.Exec(lastInsertID, balance)
-    if err != nil {
+	if err != nil {
 		fmt.Println(err)
-        return err
-    }
+		return err
+	}
 
-	fmt.Println("Account created with username & balance")
+	fmt.Printf("Account created with username %v & balance %v\n", username, balance)
 
-    return nil
+	return nil
 }
 
 func GetUserID(db *sql.DB, username string) (int64, error) {
